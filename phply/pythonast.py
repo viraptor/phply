07cd34f0e375212e94a7e42fb6860d31bacc823b
alex4men <ezh25pr@hotmail.com>
2020-02-25 17:39:50 +0500

    Added php's switch to python conversion

--- phply/pythonast.py
+++ phply/pythonast.py
@@ -321,6 +321,42 @@ def from_phpast(node):
                      list(map(to_stmt, list(map(from_phpast, deblock(node.node))))),
                      orelse, **pos(node))
 
+    
+
+    if isinstance(node, php.Switch):
+        ifexpr = py.Compare(from_phpast(node.expr), [py.Eq(**pos(node))],
+                              [from_phpast(node.nodes[0].expr)],
+                              **pos(node))
+        def recurseIfs(conds, bodies, current):
+            assert (len(conds) == len(bodies)), \
+                'Length of conds %r and bodies %r mismatch' % (len(conds), len(bodies))
+
+            if current >= len(conds):
+                return []
+            elif current == len(conds)-1:
+                return bodies[current]
+            return py.If(conds[current], 
+                         bodies[current], 
+                         recurseIfs(conds, bodies, current+1),
+                         **pos(node))
+
+        elseifs_conditions = [py.Compare(from_phpast(node.expr), [py.Eq(**pos(node))],
+                              [from_phpast(subnode.expr)],
+                              **pos(node))
+                              for subnode in node.nodes[1:]]
+        elseifs_bodies = [list(map(to_stmt, list(map(from_phpast, subnode.nodes)))) 
+                            for subnode in node.nodes[1:]]
+
+        elseifs = recurseIfs(elseifs_conditions, elseifs_bodies, 0)
+
+        return py.If(ifexpr,
+                     list(map(to_stmt, list(map(from_phpast, node.nodes[0].nodes)))),
+                     elseifs, 
+                     **pos(node))
+
+
+
+
     if isinstance(node, php.For):
         assert node.test is None or len(node.test) == 1, \
             'only a single test is supported in for-loops'
